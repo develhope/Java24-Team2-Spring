7bf05ea504ec999c5eedb2a3fd4ec6207b09aa0c
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-05-16 11:26:10 +0200

    Aggiunte dipendenze necessarie al progetto, configurata la connessione al db, inizio implementazione commenti

---
+++ src/main/java/com/develhope/spring/controllers/CommentController.java
@@ -0,0 +1,11 @@
+package com.develhope.spring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+
+    // implementare operazioni CRUD per commenti
+}
---
+++ src/main/java/com/develhope/spring/entities/Comment.java
@@ -0,0 +1,34 @@
+package com.develhope.spring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+//    @OneToOne
+//    private User user;
+    // decommentare quando viene creata la classe User
+
+//    @OneToOne
+//    private Article article;
+    // decommentare quando viene creata la classe Article
+}
